eb86736bed92232f9e3d7f3524fd9e42ab070c8c
Azat Ayupov <azat.web@gmail.com>
2021-03-09 13:58:47 +0300

    feat: solving the carbon dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,16 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LOG_OF_TWO = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity != "string") return false;
+
+  sampleActivity = parseFloat(sampleActivity);
+  if (sampleActivity > 15 ||
+      sampleActivity <= 0 ||
+      Number.isNaN(sampleActivity)) return false;
+
+  const age = Math.abs(Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD / LOG_OF_TWO);
+
+  return Math.ceil(age);
 };
