15b8a7c9c322ae58cc4a1e9bd95b18297e278303
Azat Ayupov <azat.web@gmail.com>
2021-03-09 11:34:20 +0300

    feat: implement solving of count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,10 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let catsCounter = 0;
+  for (row of matrix) {
+    for (item of row) {
+      if (item === '^^') catsCounter += 1;
+    }
+  }
+
+  return catsCounter;
 };
